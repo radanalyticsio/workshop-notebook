787d645c90fae56ea4b1a46c2b4b8a510ceb9d78
Jirka Kremser <jkremser@redhat.com>
2017-08-09 16:39:41 +0200

    minor/typo: using the right name of the file where the results were written

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-_results_file="./all-results.perf"
+_results_file="./all.perf"
 
 if [ -n "$TRAVIS_PULL_REQUEST" ] && [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -f "$_results_file" ]; then
   echo -e "Commenting on PR with the results"
